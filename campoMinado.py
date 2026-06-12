7f0dd41c05abc7d373f136a78e52a0d8321867ea
AbaEcaxiMii <andreymrigo@gmail.com>
2026-06-12 14:41:04 -0300

    comentarios

--- campoMinado.py
+++ campoMinado.py
@@ -3,32 +3,31 @@ import math
 import sys
 import random
 
-#teste teste
 def limpar(): #Apaga o que está escrito no terminal. Facilita os testes
 
     os.system('cls' if os.name == 'nt' else 'clear')
 
 limpar()
     
-def contarBombaVizinha(matriz, linha, coluna, ordem):
+def contarBombaVizinha(matriz, linha, coluna, ordem): #retorna quantas bombas tem ao redor de um ponto
     
     bombas = 0
 
     for i in range(-1, 2): # i é como o a posição x, checa se tem bomba no lado esquerdo (-1) e direito (1)
-        for j in range(-1,2):
+        for j in range(-1,2): # j é como o a posição y, checa se tem bomba abaixo (-1) e acima (1)
 
-            if i == 0 and j == 0: continue # IGNORA O CENTRO
+            if i == 0 and j == 0: continue # ignora o centro (ponto), queremos saber se tem bombas ao redor
                 
-            novaLinha, novaColuna = linha + i, coluna + j
+            novaLinha, novaColuna = linha + i, coluna + j #ponto onde será checado se tem bomba
             
-            if 0 <= novaLinha < ordem and 0 <= novaColuna < ordem:
+            if 0 <= novaLinha < ordem and 0 <= novaColuna < ordem: #Cuida para não checarem pontos fora da matriz
 
                 if matriz[novaLinha][novaColuna] == 1: bombas += 1
 
     return bombas
 
 def criarMatrizCampo(matrizOriginal, ordem):
-    matrizCampo = [[0 for _ in range(ordem)] for _ in range(ordem)]
+    matrizCampo = [[0 for _ in range(ordem)] for _ in range(ordem)] #cria uma matriz apenas com 0
     
     for i in range(ordem):
         for j in range(ordem):
@@ -41,17 +40,18 @@ def main():
 
     ordem = 5
     dificuldade = 2 * ordem
-    matrizMapa = [[0 for coluna in range(ordem)] for linha in range(ordem)]
-    quantidadeBombas = random.randint(0, dificuldade)
+    matrizMapa = [[0 for coluna in range(ordem)] for linha in range(ordem)] #cria uma matriz apenas com 0
+    quantidadeBombas = random.randint(0, dificuldade) #escolhe o numero de bombas no campo
 
-    for _ in range(quantidadeBombas):
+    for _ in range(quantidadeBombas): #escolhe aleatoriamente um espaço para ter bomba
         bombaLinha, bombaColuna = random.randint(0, ordem -1 ), random.randint(0, ordem - 1)
-        matrizMapa[bombaLinha][bombaColuna] = 1 # LUGAR ALEATÓRIO QUE TEM BOMBA
+        matrizMapa[bombaLinha][bombaColuna] = 1
     
     matrizCampo = criarMatrizCampo(matrizMapa, ordem)
+
+    #matrizMapa= matriz apenas com 0 e 1, 0 significa que o ponto não tem bomba, caso 1, então tem
+    #matrizCampo= matriz onde cada ponto reflete o numero de bombas ao redor, se o lugar tem bomba então é -1
 
     print('\n'.join(f'{str(elemento)}' for elemento in matrizCampo))
 
 if __name__ == '__main__': main()
-
-# CUZINHO PRETO
