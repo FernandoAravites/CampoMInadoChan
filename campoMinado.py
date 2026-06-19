9d30651ba9d203e720a96ce92032e041c3fde7d4
FernandoAravites <fernando.aravites@gmail.com>
2026-06-19 17:07:47 -0300

    Update campoMinado.py

--- campoMinado.py
+++ campoMinado.py
@@ -19,21 +19,22 @@ def contarBombaVizinha(matriz, linha, coluna, ordem):
 
             if i == 0 and j == 0: continue # IGNORA O CENTRO
                 
-            novaLinha, novaColuna = linha + i, coluna + j
+            novaLinha, novaColuna = linha + i, coluna + j # NOVAS POSIÇÕES PARA CHECAR SE TEM BOMBA
             
-            if 0 <= novaLinha < ordem and 0 <= novaColuna < ordem:
+            if 0 <= novaLinha < ordem and 0 <= novaColuna < ordem: # LIMITA DENTRO DA ORDEM DA MATRIZ
 
                 if matriz[novaLinha][novaColuna] == 1: bombas += 1
 
-    return bombas
+    return bombas # BOMBAS PRESENTES AO REDOR DA CASA ESOCLHIDA
 
 def criarMatrizCampo(matrizOriginal, ordem):
+
     matrizCampo = [[0 for _ in range(ordem)] for _ in range(ordem)]
     
     for i in range(ordem):
         for j in range(ordem):
-            if matrizOriginal[i][j] == 1: matrizCampo[i][j] = 'B'
-            else: matrizCampo[i][j] = contarBombaVizinha(matrizOriginal, i, j, ordem)
+            if matrizOriginal[i][j] == 1: matrizCampo[i][j] = 'B' # TROCA O VALOR 1 POR UM B ONDE TIVER BOMBA
+            else: matrizCampo[i][j] = contarBombaVizinha(matrizOriginal, i, j, ordem) # TROCA PELO VALOR DE BOMBAS NA VIZINHANÇA (CASO NÃO TENHA BOMBA NA CASA ESPECIFICAMENTE)
     
     return matrizCampo
 
@@ -46,9 +47,9 @@ def escolherDificuldade(dificuldadeEntrada):
 
 def criarMatrizFalsa(ordem):
 
-    return [['x' for coluna in range(ordem)] for linha in range(ordem)] #matriz falsa só com x
+    return [['x' for coluna in range(ordem)] for linha in range(ordem)] # matriz falsa só com x
 
-def imprimirMatriz(matriz):
+def imprimirMatriz(matriz): # GENÉRICO (SEM CORES)
 
     for coluna in matriz:
         for elemento in coluna:
@@ -59,8 +60,8 @@ def coordenadasSagradas(linhaEscolha, colunaEscolha):
 
     posicoesSagradas = []
 
-    for i in range(-1, 2): #-1 é acima
-        for j in range(-1,2): #-1 é esquerda
+    for i in range(-1, 2): # -1 é acima
+        for j in range(-1,2): # -1 é esquerda
             coordenadas = (linhaEscolha + i, colunaEscolha + j)
             posicoesSagradas.append(coordenadas)
 
@@ -69,23 +70,34 @@ def coordenadasSagradas(linhaEscolha, colunaEscolha):
 def revelarCasasProximas(linhaEscolha, colunaEscolha, ordem, matrizCampo, matrizJogador, primeira_vez):
 
     #-1:-1 I -1:0 I -1:1
-        #0:-1 I 0:0 I 0:1
-        #1:-1 I 1:0 I 1:1
-        for i in range(-1, 2): #-1 é acima
-            for j in range(-1,2): #-1 é esquerda
+    #0:-1 I 0:0 I 0:1
+    #1:-1 I 1:0 I 1:1
+
+    for i in range(-1, 2): #-1 é acima
+        for j in range(-1,2): #-1 é esquerda
+
+            if i == 0 and j == 0: continue # IGNORA O CENTRO
+                
+            novaLinha, novaColuna = linhaEscolha + i, colunaEscolha + j
+            
+            if 0 <= novaLinha < ordem and 0 <= novaColuna < ordem:
+                if i!=0 and j!=0: # checa os cantos
+                    if (matrizCampo[linhaEscolha + i][colunaEscolha] == 0 or matrizCampo[linhaEscolha][colunaEscolha + j] == 0) and matrizCampo[linhaEscolha + i][colunaEscolha + j] == 0:
+                        matrizJogador[novaLinha][novaColuna] = matrizCampo[novaLinha][novaColuna]
+                elif matrizCampo[novaLinha][novaColuna] == 0: matrizJogador[novaLinha][novaColuna] = matrizCampo[novaLinha][novaColuna]
+
+    
+    if primeira_vez:
+        for i in range(-2, 3): #-1 é acima
+            for j in range(-2,3): #-1 é esquerda
 
                 if i == 0 and j == 0: continue # IGNORA O CENTRO
-                    
-                novaLinha, novaColuna = linhaEscolha + i, colunaEscolha + j
                 
-                if 0 <= novaLinha < ordem and 0 <= novaColuna < ordem:
-                    if i!=0 and j!=0: #checa os cantos
-                        if (matrizCampo[linhaEscolha + i][colunaEscolha] == 0 or matrizCampo[linhaEscolha][colunaEscolha + j] == 0) and matrizCampo[linhaEscolha + i][colunaEscolha + j] == 0:
-                            matrizJogador[novaLinha][novaColuna] = matrizCampo[novaLinha][novaColuna]
-                    elif matrizCampo[novaLinha][novaColuna] == 0: matrizJogador[novaLinha][novaColuna] = matrizCampo[novaLinha][novaColuna]
+                novaLinha, novaColuna = linhaEscolha + i, colunaEscolha + j
+            
+                if 0 <= novaLinha < ordem and 0 <= novaColuna < ordem and matrizCampo[novaLinha][novaColuna] != "B":
 
-                    if primeira_vez:
-                        matrizJogador[novaLinha][novaColuna] = matrizCampo[novaLinha][novaColuna]
+                    matrizJogador[novaLinha][novaColuna] = matrizCampo[novaLinha][novaColuna]
 
 def imprimirMatrizColorida(matriz):
 
@@ -120,7 +132,7 @@ def main():
 
     print("Pressione Enter para continuar...", end="", flush=True)
 
-    while True:
+    while True: # LIMPA O "Pressione Enter para continuar..."
         if msvcrt.getch() == b'\r':
             break
 
@@ -132,7 +144,7 @@ def main():
 3 - difícil (15 x 15)
                                          
 """))
-    ordem = escolherDificuldade(dificuldadeEntradaPlayer)
+    ordem = escolherDificuldade(dificuldadeEntradaPlayer) # ordem da matriz quadrada do jogo
 
     limpar()
 
@@ -142,47 +154,47 @@ def main():
 
     imprimirMatriz(matrizFalsa)
 
-    linhaEscolha, colunaEscolha = map(int, input("Digite a posição para escolha (linha coluna): ").split())
-    linhaEscolha -= 1
+    linhaEscolha, colunaEscolha = map(int, input("Digite a posição para escolha (linha coluna): ").split()) # coordenada (x, y) escolhida
+    linhaEscolha -= 1 # COMPUTADOR COMEÇA A CONTAGEM NO 0, QUEREMOS QUE SEJA NO 1 PARA FICAR MAIS INTUITÍVO
     colunaEscolha -= 1
 
-    tempoInicio = time.time()
+    tempoInicio = time.time() # COMEÇA A CRONOMETRAR O TEMPO
 
-    dificuldade = 2 * ordem
+    dificuldade = 2 * ordem #DIFICULDADE LIMITE
 
-    matrizMapa = [[0 for coluna in range(ordem)] for linha in range(ordem)]
+    matrizMapa = [[0 for coluna in range(ordem)] for linha in range(ordem)] # MATRIZ COM 0s ONDE ESTIVER LIVRE E 1s ONDE TIVER BOMBA
 
     quantidadeBombas = random.randint(dificuldade // 2, dificuldade + 1)
 
-    posicoesSagradas = coordenadasSagradas(linhaEscolha, colunaEscolha)  
+    posicoesSagradas = coordenadasSagradas(linhaEscolha, colunaEscolha) # LISTA COM COORDENADAS ONDE NÃO PODE TER BOMBA (ONDE O JOGADOR CLICA E NOS ARREDORES QUE SERÃO REVELADOS)
 
-    quantidadeBombasAgora = quantidadeBombas
+    quantidadeBombasAgora = quantidadeBombas # PRECISA EXISTIR O "AGORA", POIS POSTERIORMENTE A quantidadeBombas será utilizada para confirmar a win do player
 
     while quantidadeBombasAgora > 0:
-        bombaLinha, bombaColuna = random.randint(0, ordem - 1 ), random.randint(0, ordem - 1)
-        coordenadaBomba = (bombaLinha, bombaColuna)
-        if coordenadaBomba not in posicoesSagradas:
-            matrizMapa[bombaLinha][bombaColuna] = 1
+        bombaLinha, bombaColuna = random.randint(0, ordem - 1 ), random.randint(0, ordem - 1) # ESCOLHE UM LUGAR ALEATÓRIO PARA A BOMBA
+        coordenadaBomba = (bombaLinha, bombaColuna) # CRIA UMA COORDENADA PARA TAL LUGAR
+        if coordenadaBomba not in posicoesSagradas: # TESTA SE O FORMATO COORDENADA DA BOMBA NÃO ESTÁ PRESENTE NA LISTA DE POSIÇÕES SAGRADAS
+            matrizMapa[bombaLinha][bombaColuna] = 1 # COLOCA UMA BOMBA NO LUGAR 
             quantidadeBombasAgora-=1
     
-    matrizCampo = criarMatrizCampo(matrizMapa, ordem)
-    matrizJogador = criarMatrizFalsa(ordem)
-    primeira_vez=1
+    matrizCampo = criarMatrizCampo(matrizMapa, ordem) # MATRIZ QUE POSSUI TODAS AS INFORMAÇÕES DE JOGO
+    matrizJogador = criarMatrizFalsa(ordem) # MATRIZ QUE SÓ O PLAYER VAI VER
+    primeira_vez = 1 # IMPEDIR QUE ELE PERGUNTE NOVAMENTE AS POSIÇÕES
     
     while True:
         if not primeira_vez:
-            linhaEscolha, colunaEscolha = map(int, input("Digite a posição para escolha (linha coluna): ").split())
+            linhaEscolha, colunaEscolha = map(int, input("Digite a posição para escolha: ").split())
             linhaEscolha -= 1
             colunaEscolha -= 1
 
-        os.system("cls")  # limpa a tela inteira
+        limpar()
 
-        matrizJogador[linhaEscolha][colunaEscolha] = matrizCampo[linhaEscolha][colunaEscolha]
+        matrizJogador[linhaEscolha][colunaEscolha] = matrizCampo[linhaEscolha][colunaEscolha] # TROCA O X DA MATRIZ JOGADOR PELO VALOR QUE EXISTE NA matrizCampo
 
-        revelarCasasProximas(linhaEscolha, colunaEscolha, ordem, matrizCampo, matrizJogador, primeira_vez)
+        revelarCasasProximas(linhaEscolha, colunaEscolha, ordem, matrizCampo, matrizJogador, primeira_vez) # FUNÇÃO QUE REVELA AS CASAS PRÓXIMAS DA ESCOLHIDA
 
-        x_sobrando = 0
-        for coluna in matrizJogador:
+        x_sobrando = 0 # VARIÁVEL QUE DEFINE O AVANÇO DO PLAYER NA PARTIDA (QUANTIDADE DE LUGARES NÃO DESCOBERTOS)
+        for coluna in matrizJogador: # IMPRIMIR matrizJogador COM CORES INDICATIVAS
             for elemento in coluna:
 
                 match elemento:
@@ -194,42 +206,41 @@ def main():
                     case 4: print(Fore.BLACK + str(elemento), end=" ")
                     case 'x':
                         print(Fore.WHITE + elemento, end=" ")
-                        x_sobrando += 1
-            print(Style.RESET_ALL)
+                        x_sobrando += 1 # AUMENTA PARA CADA RETIRADA DE x
+            print(Style.RESET_ALL) # TROCA A COR GERAL
             
         if debugMode:
             print("Debug matriz / x's sobrando:")
-            imprimirMatrizColorida(matrizCampo)
+            imprimirMatrizColorida(matrizCampo) #FUNÇÃO GENÉRICA UTILIZADA PARA EVITAR LINHA DE CÓDIGO NO MEIO DO main()
             print(Style.RESET_ALL)
             print(x_sobrando)
-        primeira_vez=0
+        primeira_vez = 0
 
-        if matrizCampo[linhaEscolha][colunaEscolha] == 'B':
+        if matrizCampo[linhaEscolha][colunaEscolha] == 'B': # CONDIÇÃO DE DERROTA -> ESCOLHEU POSIÇÃO COM BOMBA
             print()
             print(Fore.RED + "Você perdeu. Como a matriz era:")
             imprimirMatrizColorida(matrizCampo)
-
             break
-        elif x_sobrando==quantidadeBombas:
+        elif x_sobrando == quantidadeBombas: # CONDIÇÃO DE VITÓRIA -> LUGARES NÃO DESCOBERTOS = QUANTIDADE DE BOMBAS, OU SEJA, SABE O LOCAL DE TODAS AS BOMBAS
             print(Fore.GREEN + "Você ganhou yayyy :D", end=" ")
             break
     print()
-    tempoJogado(tempoInicio)
+    tempoJogado(tempoInicio) # MOSTRA O TEMPO QUE O JOGADOR DEMOROU PARA TERMINAR A PARTIDA (VITÓRIA OU DERROTA)
 
 while True:
 
     if __name__ == "__main__":
-        try:
+        try: # TENTA RODAR O CÓDIGO NORMALMENTE (RESETANDO A FONTE)
             main()
 
             print(Style.RESET_ALL)
 
             resposta = input(
-                "Gostaria de encerrar a sessão? (digite 'sim')\n"
+                "Gostaria de encerrar a partída? (digite 'sim')\n"
             ).lower()
 
             os.system("cls")
 
             if resposta == "sim": sys.exit()
 
-        except: sys.exit()
+        except: sys.exit() # ENCERRA O PROGRMA SE DER ERRO
